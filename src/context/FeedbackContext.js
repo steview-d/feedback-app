00a88e876cc7f43669f78443878e82deaf92dda7
Dave Laffan <dave.laffan@outlook.com>
2022-01-08 23:26:01 +0000

    Update with content up to s6v35

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -5,39 +5,64 @@ const FeedbackContext = createContext();
 export const FeedbackProvider = ({ children }) => {
   const [feedback, setFeedback] = useState([
     {
-        id: 1,
-        rating: 10,
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-      },
-      {
-        id: 2,
-        rating: 9,
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-      },
-      {
-        id: 3,
-        rating: 8,
-        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
-      },
+      id: 1,
+      rating: 10,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 2,
+      rating: 9,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
+    {
+      id: 3,
+      rating: 8,
+      text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. consequuntur vel vitae commodi alias voluptatem est voluptatum ipsa quae.',
+    },
   ]);
 
+  const [feedbackEdit, setFeedbackEdit] = useState({
+    item: {},
+    edit: false,
+  });
+
+  // Add feedback
   const addFeedback = newFeedback => {
     newFeedback.id = uuidv4();
     setFeedback([newFeedback, ...feedback]);
   };
 
+  // Delete feedback
   const deleteFeedback = id => {
     if (window.confirm('Are you sure you want to delete?')) {
       setFeedback(feedback.filter(item => item.id !== id));
     }
   };
 
+  // Update feedback item
+  const updateFeedback = (id, updItem) => {
+    setFeedback(
+      feedback.map(item => (item.id === id ? { ...item, ...updItem } : item))
+    );
+  };
+
+  // Set item to be updated
+  const editFeedback = item => {
+    setFeedbackEdit({
+      item,
+      edit: true,
+    });
+  };
+
   return (
     <FeedbackContext.Provider
       value={{
         feedback,
-        deleteFeedback,
+        feedbackEdit,
         addFeedback,
+        deleteFeedback,
+        editFeedback,
+        updateFeedback,
       }}
     >
       {children}
