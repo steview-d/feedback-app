e440f6066f4da18135d6f0cb6a8975fe62415afd
Dave Laffan <dave.laffan@outlook.com>
2022-01-08 13:51:56 +0000

    Update with content up to s3v24

--- src/components/FeedbackForm.jsx
+++ src/components/FeedbackForm.jsx
@@ -3,32 +3,46 @@ import RatingSelect from './RatingSelect';
 import Card from './shared/Card';
 import Button from './shared/Button';
 
-function FeedbackForm() {
+function FeedbackForm({ handleAdd }) {
   const [text, setText] = useState('');
   const [rating, setRating] = useState(10);
   const [btnDisabled, setBtnDisabled] = useState(true);
   const [message, setMessage] = useState('');
 
   const handleTextChange = ({ target: { value } }) => {
-    if(value === '') {
-        setBtnDisabled(true)
-        setMessage(null)
+    if (value === '') {
+      setBtnDisabled(true);
+      setMessage(null);
     } else if (value.trim().length < 10) {
-        setBtnDisabled(true)
-        setMessage('Text must be at least 10 characters')
+      setBtnDisabled(true);
+      setMessage('Text must be at least 10 characters');
     } else {
-        setBtnDisabled(false)
-        setMessage(null)
+      setBtnDisabled(false);
+      setMessage(null);
     }
 
-      setText(value)
-  }
+    setText(value);
+  };
+
+  const handleSubmit = e => {
+    e.preventDefault();
+    if (text.trim().length > 10) {
+      const newFeedback = {
+        text,
+        rating,
+      };
+
+      handleAdd(newFeedback)
+
+      setText('')
+    }
+  };
 
   return (
     <Card>
-      <form>
+      <form onSubmit={handleSubmit}>
         <h2>How would you rate your service with us?</h2>
-        <RatingSelect select={(rating) => setRating(rating)} />
+        <RatingSelect select={rating => setRating(rating)} />
         {/* @todo - rating select component */}
         <div className='input-group'>
           <input
@@ -37,9 +51,11 @@ function FeedbackForm() {
             placeholder='Write a review'
             value={text}
           />
-          <Button type='submit' isDisabled={btnDisabled}>Send</Button>
+          <Button type='submit' isDisabled={btnDisabled}>
+            Send
+          </Button>
         </div>
-        {message && <div className='message'>{message}</div> }
+        {message && <div className='message'>{message}</div>}
       </form>
     </Card>
   );
