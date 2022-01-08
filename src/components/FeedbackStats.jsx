172018bee715e231bb8733c0cf2b44bba13e6deb
Dave Laffan <dave.laffan@outlook.com>
2022-01-08 16:24:18 +0000

    Update with content up to s6v32

--- src/components/FeedbackStats.jsx
+++ src/components/FeedbackStats.jsx
@@ -1,7 +1,9 @@
-import PropTypes from 'prop-types'
+import {useContext} from 'react'
+import FeedbackContext from '../context/FeedbackContext';
 
+function FeedbackStats() {
+  const {feedback} = useContext(FeedbackContext)
 
-function FeedbackStats({ feedback }) {
   // Calculate ratings avg
   let average = feedback.reduce((acc, cur) => {
     return acc + cur.rating
@@ -15,10 +17,6 @@ function FeedbackStats({ feedback }) {
       <h4>Average Rating: {isNaN(average) ? 0 : average}</h4>
     </div>
   );
-}
-
-FeedbackStats.prototype = {
-    feedback: PropTypes.array.isRequired,
 }
 
 export default FeedbackStats;
